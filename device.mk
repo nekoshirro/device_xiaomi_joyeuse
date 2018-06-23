b877d85e6bd091cb0f8c0b9812f07a6cdd675774
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    joyeuse: Build separate system and vendor HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -20,7 +20,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
