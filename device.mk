be8220f07b996811e44b71988c89d31e4c9bf2f8
Volodymyr Zhdanov <wight554@gmail.com>
2020-11-22 00:25:17 +0100

    Revert "joyeuse: Get rid of megvii dependencies in `camera.qcom.so`"
    * doesn't work anymore
    
    This reverts commit f8e46f50224ab79fa52f54976c4967dd0f0bec1f.
    
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -89,10 +89,9 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
-    Snap \
+    Snap
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    libshim_megvii
+    android.hardware.camera.provider@2.4-service_64
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
