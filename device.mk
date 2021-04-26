0d410d753d07183f9ae709b4192a42019d1a6f2d
Alex Winkowski <dereference23@outlook.com>
2021-04-26 18:32:02 +0200

    joyeuse: rootdir: Import USB init scripts from stock
    * qssi-user-11-RKQ1.200826.002-V12.0.1.7.RJWMIXM-release-keys
    
    To fix Mass Storage mode.
    Basically, this reverts commit 6bb87160375c4affbec396d2cf2be3d6a0e877f8.
    
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -193,6 +193,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc
     $(LOCAL_PATH)/rootdir/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.post_boot.sh \
+    $(LOCAL_PATH)/rootdir/bin/init.qcom.usb.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.usb.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qti.chg_policy.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.chg_policy.sh \
     $(LOCAL_PATH)/rootdir/etc/init.device.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.device.rc
