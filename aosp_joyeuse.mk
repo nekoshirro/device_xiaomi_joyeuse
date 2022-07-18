4bae3bbba3349d7edfb223e6bd9a76fc4c01bf87
nekoshirro <ais.muzakky@gmail.com>
2022-07-18 16:19:11 +0700

    joyeuse: Initial device tree
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

---
+++ aosp_joyeuse.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2022 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from joyeuse device tree
+$(call inherit-product, device/xiaomi/joyeuse/device.mk)
+
+# Inherit AOSP ROM configuration
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit MiuiCamera 12 for joyeuse
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identity configuration
+PRODUCT_NAME := aosp_joyeuse
+PRODUCT_DEVICE := joyeuse
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 9 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003 8671607 release-keys"
+
+BUILD_FINGERPRINT := google/raven/raven:12/SQ3A.220705.003/8671607:user/release-keys
