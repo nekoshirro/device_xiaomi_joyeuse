9e1166cce6c383b55a71acc1a8c98d1b548b5b7f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-21 22:02:21 +0700

    joyeuse: Drop power AIDL extension
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,9 +165,6 @@ BOARD_USES_QCOM_HARDWARE := true
 BOARD_VENDOR := xiaomi
 TARGET_BOARD_PLATFORM := atoll
 
-# Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
