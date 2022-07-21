345b82d8c70babaacdb9f33c6d112be84bc767dc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-21 18:20:38 +0700

    joyeuse: Build IPACM libs
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -187,6 +187,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal
+
 # NFC
 PRODUCT_PACKAGES += \
     NfcNci \
