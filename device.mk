ab457028fe6bee3fa658c72ac547b91667cb7ca9
nekoshirro <ais.muzakky@gmail.com>
2022-07-21 20:52:21 +0700

    joyeuse: Update sensors service to 2.1
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -355,7 +355,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
 # Servicetracker
