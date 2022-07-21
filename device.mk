96e2f769d72ab76805d4ee24634757f303d5ebc5
Alexander Winkowski <dereference23@outlook.com>
2022-07-21 18:52:07 +0700

    joyeuse: Build Xiaomi power AIDL HAL
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -314,6 +314,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service.xiaomi-libperfmgr
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
@@ -331,7 +335,10 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/xiaomi
 
 # Thermal
 PRODUCT_COPY_FILES += \
