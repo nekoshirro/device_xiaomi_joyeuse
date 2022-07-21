10ce9866203df4612139c024944685ef82c8745b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-21 09:55:47 +0700

    joyeuse: Initial lineage-sdk overlays
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -17,7 +17,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
