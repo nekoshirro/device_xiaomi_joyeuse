334fe78ae727b3eec4cd2b8559dd73717667b4db
nekoshirro <ais.muzakky@gmail.com>
2022-07-26 15:21:59 +0700

    joyeuse: Reinitialize init library * Fix error build because decommonize tree
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

--- libinit/init.cpp
+++ libinit/init.cpp
@@ -1,23 +1,25 @@
-// Copyright (C) 2022 Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#include <android-base/properties.h>
-#include <sys/sysinfo.h>
+/*
+ * Copyright (C) 2022 Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <cstdlib>
 #include <cstring>
+#include <sys/sysinfo.h>
 #include <vector>
+
+#include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
@@ -26,47 +28,16 @@
 
 using android::base::GetProperty;
 
-std::vector<std::string> ro_props_default_source_order = {
-    "", "odm.", "product.", "system.", "vendor.", "system_ext.",
-};
+void property_override(char const prop[], char const value[], bool add = true);
 
-void property_override(char const prop[], char const value[], bool add = true) {
-    auto pi = (prop_info *)__system_property_find(prop);
+void property_override(char const prop[], char const value[], bool add)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
 
     if (pi != nullptr) {
         __system_property_update(pi, value, strlen(value));
     } else if (add) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
-    }
-}
-
-void set_ro_build_prop(const std::string &source, const std::string &prop,
-                       const std::string &value, bool product = false) {
-    std::string prop_name;
-
-    if (product) {
-        prop_name = "ro.product." + source + prop;
-    } else {
-        prop_name = "ro." + source + "build." + prop;
-    }
-
-    property_override(prop_name.c_str(), value.c_str(), false);
-}
-
-void set_device_props(const std::string brand, const std::string device, const std::string model) {
-    for (const auto &source : ro_props_default_source_order) {
-        set_ro_build_prop(source, "brand", brand, true);
-        set_ro_build_prop(source, "device", device, true);
-        set_ro_build_prop(source, "model", model, true);
-    }
-}
-
-void load_device_properties() {
-    std::string hwname = GetProperty("ro.boot.hwname", "");
-    std::string region = GetProperty("ro.boot.hwc", "");
-
-    if (hwname == "joyeuse") {
-        set_device_props("Redmi", "joyeuse", "Redmi Note 9 Pro");
     }
 }
 
@@ -107,6 +78,46 @@ void load_dalvik_properties() {
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+}
+
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "vendor.",
+    "system_ext.",
+};
+
+void set_ro_build_prop(const std::string &source, const std::string &prop,
+        const std::string &value, bool product = false) {
+    std::string prop_name;
+
+    if (product) {
+        prop_name = "ro.product." + source + prop;
+    } else {
+        prop_name = "ro." + source + "build." + prop;
+    }
+
+    property_override(prop_name.c_str(), value.c_str(), false);
+}
+
+void set_device_props(const std::string brand, const std::string device, const std::string model) {
+    for (const auto &source : ro_props_default_source_order) {
+        set_ro_build_prop(source, "brand", brand, true);
+        set_ro_build_prop(source, "device", device, true);
+        set_ro_build_prop(source, "model", model, true);
+    }
+}
+
+void load_device_properties() {
+    std::string hwname = GetProperty("ro.boot.hwname", "");
+    std::string region = GetProperty("ro.boot.hwc", "");
+
+    if (hwname == "joyeuse") {
+        set_device_props(
+                "Redmi", "joyeuse", "Redmi Note 9 Pro");
+    }
 }
 
 void vendor_load_properties() {
