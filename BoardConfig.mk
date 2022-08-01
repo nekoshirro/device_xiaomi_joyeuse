9ed72479d061526d3dec2d3d17c93674cc703ceb
Hafidz Muzakky <ais.muzakky@gmail.com>
2022-08-02 05:58:25 +0700

    joyeuse: Define init library
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,10 @@ endif
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_joyeuse
+TARGET_RECOVERY_DEVICE_MODULES := libinit_joyeuse
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
