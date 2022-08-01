1820d427b62e17c35fb493c3d16207bc277b2d7d
Demon000 <demonsingur@gmail.com>
2022-08-01 08:26:07 +0700

    joyeuse: Import stock manifest
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,9 @@ ifeq ($(HOST_OS),linux)
   endif
 endif
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
