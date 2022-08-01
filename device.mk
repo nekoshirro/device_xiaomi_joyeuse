90a05ac2959361c040ec5c8ec6f7d7a973a01460
Demon000 <demonsingur@gmail.com>
2022-08-01 07:45:56 +0700

    joyeuse: Define soong namespaces
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -4,5 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit proprietary targets
 $(call inherit-product-if-exists, vendor/xiaomi/joyeuse/joyeuse-vendor.mk)
