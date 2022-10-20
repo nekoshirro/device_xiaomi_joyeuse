markakash/device_xiaomi_raphael

commit 06b604b6bef40abdb4b507fc13debb2146a922e1

Decrease the console log level
Decrease the log level to '3', which would only log KERN_ERR
and lower, onto the console. This is to avoid false-positives
in the test cases that may emit excessive logs, ultimately
leading to a watchdog bark.

Change-Id: I7967272de8e425e93035cd937982bfb3aee19597

rootdir/etc/init.qcom.rc
@@ -176,9 +176,9 @@ on boot
     # an ack packet comes out of order
     write /proc/sys/net/netfilter/nf_conntrack_tcp_be_liberal 1
 
-    # Set the console loglevel to < KERN_INFO
+    # Set the console loglevel to < KERN_WARN
     # Set the default message loglevel to KERN_INFO
-    write /proc/sys/kernel/printk "6 6 1 7"
+    write /proc/sys/kernel/printk "3 6 1 7"
 
     # Allow access for CCID command/response timeout configuration
     chown system system /sys/module/ccid_bridge/parameters/bulk_msg_timeout

rootdir/etc/init.qcom.sh
@@ -456,10 +456,10 @@ buildvariant=`getprop ro.build.type`
 case "$buildvariant" in
     "userdebug" | "eng")
         #set default loglevel to KERN_INFO
-        echo "6 6 1 7" > /proc/sys/kernel/printk
+        echo "3 6 1 7" > /proc/sys/kernel/printk
         ;;
     *)
         #set default loglevel to KERN_WARNING
-        echo "4 4 1 4" > /proc/sys/kernel/printk
+        echo "3 4 1 4" > /proc/sys/kernel/printk
         ;;
 esac

nekoshirro/device_xiaomi_joyeuse

commit 6d02a4343733153a45854fded6c4818fc1fa35f8

joyeuse: rootdir: Increase the console log level
Increase the log level to '4', which would only log KERN_ERR
and lower, onto the console.Earlier it was incorrectly reduced
to 3 which would only log KERN_CRIT and lower.

Fixes: 06b604b6bef40abdb4b507fc13debb2146a922e1("Decrease the console log level")
Change-Id: Ic87d037450dd49138c3208e1b9f844469550364b
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

rootdir/bin/init.qcom.sh
@@ -50,3 +50,17 @@ if [ ! -f /vendor/firmware_mnt/verinfo/ver_info.txt -o "$prev_version_info" != "
 fi
 chmod g-w /data/vendor/modem_config
 setprop ro.vendor.ril.mbn_copy_completed 1
+
+#check build variant for printk logging
+#current default minimum boot-time-default
+buildvariant=`getprop ro.build.type`
+case "$buildvariant" in
+    "userdebug" | "eng")
+        #set default loglevel to KERN_INFO
+        echo "4 6 1 7" > /proc/sys/kernel/printk
+        ;;
+    *)
+        #set default loglevel to KERN_WARNING
+        echo "4 4 1 4" > /proc/sys/kernel/printk
+        ;;
+esac

rootdir/etc/init.qcom.rc
@@ -192,7 +192,7 @@ on boot
 
     # Set the console loglevel to < KERN_INFO
     # Set the default message loglevel to KERN_INFO
-    write /proc/sys/kernel/printk "6 6 1 7"
+    write /proc/sys/kernel/printk "4 6 1 7"
 
     # Allow access for CCID command/response timeout configuration
     chown system system /sys/module/ccid_bridge/parameters/bulk_msg_timeout