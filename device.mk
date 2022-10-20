ca92b8c2743ba87c111dafac48ceb35eff880f00
Hafidz Muzakky <ais.muzakky@gmail.com>
2022-10-20 10:39:09 +0700

    joyeuse: Enforce interface product partition
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -13,6 +13,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# Enforce native interfaces of product partition as VNDK
+PRODUCT_PRODUCT_VNDK_VERSION := current
+
+# Enforce java interfaces of product partition
+PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
