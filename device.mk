7795f5a9712fe5fbf9cbec74a63ea17289d7bfc1
Mathieu Chartier <mathieuc@google.com>
2023-01-26 17:43:42 +0800

    joyeuse: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -457,6 +457,10 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/nxp \
     kernel/xiaomi/sm6250
 
+
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Shim
 PRODUCT_PACKAGES += \
     libwatermark_shim
