0a36f081a204ef900256a63114be1e7007dd9408
userariii <abhattacharjee717@gmail.com>
2023-07-03 15:52:12 +0000

    joyeuse: Organized props in proper path
    Signed-off-by: userariii <abhattacharjee717@gmail.com>
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,11 +183,11 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_BOARD_PLATFORM := atoll
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/properties/system_ext.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
