80945641a5a20925a128fafd4004e0fef52b0d8f
AdarshGrewal <adarshgrewal@gmail.com>
2025-12-06 14:24:34 +0700

    joyeuse: Inherit MiuiCamera if exists
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -161,6 +161,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera-joyeuse/MiuiCamera.mk)
+$(call soong_config_set,camera,package_name,com.android.camera)
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
